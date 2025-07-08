b9b27e03c01bbed0741c5423117de80d7a8616d9
dewoosin <fatal.jay@gmail.com>
2025-07-08 17:01:14 +0900

    test

--- ghosType/esp32/src/main.cpp
+++ ghosType/esp32/src/main.cpp
@@ -21,6 +21,10 @@ USBHIDKeyboard keyboard;
 #define RX_CHAR_UUID        "6e400002-b5a3-f393-e0a9-e50e24dcca9e"
 #define TX_CHAR_UUID        "6e400003-b5a3-f393-e0a9-e50e24dcca9e"
 
+// 함수 선언
+void processTypingCommand(std::string command);
+void typeText(std::string text);
+
 class MyServerCallbacks: public BLEServerCallbacks {
     void onConnect(BLEServer* pServer) {
         deviceConnected = true;
