bba650fab5ed7bf53d3566893f13fa396678d155
dewoosin <fatal.jay@gmail.com>
2025-07-08 12:51:54 +0900

    test

--- ghosType/esp32/src/main.cpp
+++ ghosType/esp32/src/main.cpp
@@ -1,34 +1,14 @@
-/**
- * @file simple_ble_test.cpp
- * @brief 가장 단순한 BLE 광고 테스트
- * 
- * 이전에 작동했던 최소한의 코드로 테스트
- */
-
 #include <Arduino.h>
-#include <NimBLEDevice.h>
 
 void setup() {
-    // 최소한의 초기화
-    delay(1000);
-    
-    // BLE 초기화 - 가장 기본적인 설정만
-    NimBLEDevice::init("GHOSTYPE");
-    
-    // 서버 생성
-    NimBLEServer* pServer = NimBLEDevice::createServer();
-    
-    // 서비스 생성
-    NimBLEService* pService = pServer->createService("12345678-1234-5678-9012-123456789abc");
-    pService->start();
-    
-    // 광고 시작
-    NimBLEAdvertising* pAdvertising = NimBLEDevice::getAdvertising();
-    pAdvertising->addServiceUUID("12345678-1234-5678-9012-123456789abc");
-    pAdvertising->start();
+    // 아무것도 하지 않음
+    pinMode(2, OUTPUT);  // LED 핀만 설정
 }
 
 void loop() {
-    // 아무것도 하지 않음
-    delay(2000);
+    // LED 깜빡임으로 정상 작동 확인
+    digitalWrite(2, HIGH);
+    delay(1000);
+    digitalWrite(2, LOW);
+    delay(1000);
 }
