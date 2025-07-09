f00467799d09c03b0264c2e5baf12dbb5ecb1f82
dewoosin <fatal.jay@gmail.com>
2025-07-09 11:10:07 +0900

    test

--- ghosType/esp32/src/main.cpp
+++ ghosType/esp32/src/main.cpp
@@ -30,10 +30,10 @@ bool deviceConnected = false;
 std::queue<String> typingQueue;
 SemaphoreHandle_t queueMutex;
 
-// BLE UUID - JavaScript와 일치
-#define SERVICE_UUID        "12345678-1234-5678-9012-123456789abc"
-#define RX_CHAR_UUID        "12345678-1234-5678-9012-123456789abd"
-#define TX_CHAR_UUID        "12345678-1234-5678-9012-123456789abe"
+// BLE UUID
+#define SERVICE_UUID        "6e400001-b5a3-f393-e0a9-e50e24dcca9e"
+#define RX_CHAR_UUID        "6e400002-b5a3-f393-e0a9-e50e24dcca9e"
+#define TX_CHAR_UUID        "6e400003-b5a3-f393-e0a9-e50e24dcca9e"
 
 // 타이핑 상태
 bool isTyping = false;
